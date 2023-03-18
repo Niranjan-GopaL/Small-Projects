37b5cc4dc7aea0f2c0a6adf27cb3a72bc785555d
Niranjan-GopaL <niranjangopalpvl@gmail.com>
2023-03-18 07:54:18 +0530

    Starter files

---
+++ Tick Tack Toe/tick-tack-toe-vite/src/components/Board.jsx
@@ -0,0 +1,13 @@
+
+
+const Board = () => {
+  return (
+    <div className="board">
+      <div className="board-row"></div>
+      <div className="board-row"> </div>
+      <div className="board-row"></div>
+    </div>
+  )
+}
+
+export default Board
---
+++ Tick Tack Toe/tick-tack-toe-vite/src/components/Square.jsx
@@ -0,0 +1,11 @@
+const Square = (props) =>{
+    // prop is OBJECT that is passed to the component
+    console.log(prop);
+    // prop.value would print string 5
+    console.log(prop.value);
+    return (
+        <p>Header Section goes here</p>
+    )
+}
+
+export default Square;
