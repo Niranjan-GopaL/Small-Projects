572fa0f673462a1084bf28ca72ea6cb7c48e7006
Niranjan-GopaL <niranjangopalpvl@gmail.com>
2023-03-19 08:44:46 +0530

    Close to finishing

--- GoogleSearchFromTerminal/main.py
+++ GoogleSearchFromTerminal/main.py
@@ -1,11 +1,40 @@
 import webbrowser
 import sys
 
-url = 'http://docs.python.org/'
+BASE_URL = 'https://www.google.com/search?q='
 
-# Windows
+valid_websites =[
+    'reddit.com',
+    'stackoverflow.com',
+    'youtube.com',
+    'stackexchange.com',
+    'medium.com',
+]
+
 chrome_path = 'C:/Program Files/Google/Chrome/Application/chrome.exe %s'
 
-webbrowser.get(chrome_path).open(url)
 
-print(sys.argv)
+def create_query():
+    # print(sys.argv) # prints a list of arguments that we write with python3 in terminal
+    
+    query = sys.argv[1:]
+    return "+".join(query)
+
+
+def create_filter_for_querying():
+    filter  = '('
+    for index, website in enumerate(valid_websites):
+        filter += 'site:' + website 
+        if index != len(valid_websites) - 1:
+            filter += 'OR'
+        else:
+            filter += ')'
+    return filter
+        
+def create_url(): 
+    final_url = BASE_URL + create_query() + create_filter_for_querying()
+    return final_url
+
+
+webbrowser.get(chrome_path).open(create_url())
+
