28783ad8efa2c2829109fc6f4a0e7fc3359ef87e
Niranjan-GopaL <niranjangopalpvl@gmail.com>
2023-06-03 08:46:09 +0530

    changed the way app.jsx was made , I HATE the initial way function def, I need organised multi exportable fucntion

--- mastering_tailwindcss/src/App.jsx
+++ mastering_tailwindcss/src/App.jsx
@@ -1,10 +1,10 @@
-export default App = () => {
+export const App =  ()=> {
 
   return (
     <>
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
     </>
   )
 }
