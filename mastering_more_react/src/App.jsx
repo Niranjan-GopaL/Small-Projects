de16d46b26fdf15a57da426673e2cfd92726a159
Niranjan-GopaL <niranjangopalpvl@gmail.com>
2023-08-01 14:31:37 +0530

    Use Case of rendering unknow html inside a Component

--- mastering_more_react/src/App.jsx
+++ mastering_more_react/src/App.jsx
@@ -1,34 +1,21 @@
 import { useState } from 'react'
 import './App.css'
 import Component from './Props'
+import UseCase from './Import_Use_Case'
 
 function App() {
 
   return (
     <>
-        <Component 
-            name="Name_1" 
-            alias="ALias_1" 
-            note="YOU CAN PASS ANYTHING any no of times"
-            note2="YOU CAN PASS ANYTHING any no of times"
-        />
+        {/* THE COMPONENT has unknow html stuff to be rendered inside component */}
+        <UseCase>
 
-        <Component 
-            name="Name_2" 
-            alias="ALias_2" 
-            note="YOU CAN PASS ANYTHING any no of times"
-            note2="YOU CAN PASS ANYTHING any no of times"
-        />
-
-        <Component 
-            name="Name_3" 
-            alias="ALias_4" 
-            note="YOU CAN PASS ANYTHING any no of times"
-            note2="YOU CAN PASS ANYTHING any no of times"
-        />
-
-
-
+            <div>
+                <div>
+                </div>
+            </div>
+            
+        </UseCase>
     </>
   )
 }
--- mastering_more_react/src/Import_Use_Case.jsx
+++ mastering_more_react/src/Import_Use_Case.jsx
@@ -5,6 +5,7 @@ const UseCase = (props) => (
         <h1>Hello World</h1>
 
         {/* WHATEVER HTML needs to be written inside the UseCase */}
+        {/* All those divs are rendered here*/}
         {props.children}
     </div>
 )
